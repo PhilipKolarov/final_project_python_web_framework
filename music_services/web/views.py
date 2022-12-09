f139a1f0c1d4ffde3cd41cd3aa25279943943f64
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2022-12-09 20:00:21 +0200

    Update views.py

--- music_services/web/views.py
+++ music_services/web/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView
+
+from music_services.web.forms import RecommendationCreateForm, ServiceCreateForm, ServiceEditForm, ServiceDeleteForm
 from music_services.web.models import Profile, Service, Review
+
+
+def get_profile():
+    try:
+        return Profile.objects.get()
+    except:
+        return None
+
+
+def index(request):
+    profile = get_profile()
+
+    context = {
+        'profile': profile,
+    }
+
+    return render(
+        request,
+        'index.html',
+        context
+    )
 
 
 class ProfileCreateView(CreateView):
@@ -19,19 +42,70 @@ class ProfileDeleteView(DeleteView):
     success_url = reverse_lazy('success') # This is an HTML to which we will be redirected upon successful deletion
 
 
-class ServiceCreateView(CreateView):
-    model = Service
-    fields = ['type', 'description', 'image_url', 'price']
+def service_create(request):
+    if request.method == 'GET':
+        form = ServiceCreateForm()
+    else:
+        form = ServiceCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('profile details')
+
+    context = {
+        'form': form,
+    }
+
+    return render(
+        request,
+        'services-create.html',
+        context
+    )
 
 
-class ServiceEditView(UpdateView):
-    model = Service
-    fields = '__all__'
+def service_edit(request, pk):
+    service = Service.objects.filter(pk=pk).get()
+
+    if request.method == 'GET':
+        form = ServiceEditForm(instance=service)
+    else:
+        form = ServiceEditForm(request.POST, instance=service)
+        if form.is_valid():
+            form.save()
+            return redirect('services details')
+
+    context = {
+        'form': form,
+        'service': service,
+    }
+
+    return render(
+        request,
+        'services-edit.html',
+        context
+    )
 
 
-class ServiceDeleteView(DeleteView):
-    model = Service
-    success_url = reverse_lazy('success') # Same as Profile Deletion
+def service_delete(request, pk):
+    service = Service.objects.filter(pk=pk).get()
+
+    if request.method == 'GET':
+        form = ServiceDeleteForm(instance=service)
+    else:
+        form = ServiceDeleteForm(request.POST, instance=service)
+        if form.is_valid():
+            form.save()
+            return redirect('catalogue')
+
+    context = {
+        'form': form,
+        'service': service,
+    }
+
+    return render(
+        request,
+        'services-delete.html',
+        context
+    )
 
 
 class ReviewCreateView(CreateView):
@@ -42,3 +116,35 @@ class ReviewCreateView(CreateView):
 class ReviewDeleteView(DeleteView):
     model = Review
     success_url = reverse_lazy('success') # Same as Profile/Service Deletion
+
+
+# TODO Include accurate htmls, pages, and/or forms once written
+def recommendation_create(request):
+    if request.method == 'GET':
+        form = RecommendationCreateForm()
+    else:
+        form = RecommendationCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('about')
+
+    context = {
+        'form': form,
+    }
+
+    return render(
+        request,
+        '',
+        context
+    )
+
+
+def about(request):
+    context = {
+    }
+
+    return render(
+        request,
+        'about.html',
+        context
+    )
