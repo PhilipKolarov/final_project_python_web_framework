0fe0a6dad90555e00ca109ee5c7b5ea482c9998b
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2022-12-12 23:00:32 +0200

    Update urls.py

--- music_services/web/urls.py
+++ music_services/web/urls.py
@@ -1,23 +1,24 @@
 from django.urls import path, include
 
 from music_services.web.views import index, ProfileCreateView, ProfileDeleteView, ProfileEditView, service_create, \
-    service_edit, service_delete, ReviewCreateView, ReviewDeleteView, recommendation_create
+    service_edit, service_delete, ReviewCreateView, ReviewDeleteView, recommendation_create, about, service_details, \
+    ProfileDetailsView, catalogue
 
 urlpatterns = [
     path('', index, name='index'),
     path('about', about, name='about'),
     path('profile/', include([
         path('create/', ProfileCreateView.as_view(), name='profile create'),
-        path('details/', profile_details, name='profile details'),
-        path('edit/', ProfileEditView.as_view(), name='profile edit'),
-        path('delete/', ProfileDeleteView.as_view(), name='profile delete'),
+        path('details/<int:pk>/', ProfileDetailsView.as_view(), name='profile details'),
+        path('edit/<int:pk>/', ProfileEditView.as_view(), name='profile edit'),
+        path('delete/<int:pk>/', ProfileDeleteView.as_view(), name='profile delete'),
     ])),
-    path('services/', include([
-        path('', services, name='services'),
-        path('create/', service_create, name='services create'),
-        path('details/<int:pk>/', service_details, name='services details'),
-        path('edit/<int:pk>/', service_edit, name='services edit'),
-        path('delete/<int:pk>/', service_delete, name='services delete'),
+    path('catalogue', catalogue, name='catalogue'),
+    path('service/', include([
+        path('create/', service_create, name='service create'),
+        path('details/<int:pk>/', service_details, name='service details'),
+        path('edit/<int:pk>/', service_edit, name='service edit'),
+        path('delete/<int:pk>/', service_delete, name='service delete'),
     ])),
     path('review/', include([
         path('create/', ReviewCreateView.as_view(), name='review create'),
